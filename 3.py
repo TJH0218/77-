e30b874cfb0357788abb397983eb39638922922b
TJH0218 <c110156154@nkust.edu.tw>
2022-04-08 16:34:17 +0800

    77

---
+++ 3.py
@@ -0,0 +1,4 @@
+a = int(input("請輸入西元年:"))
+d = {0:"monkey",1:"rooster",2:"dog",3:"pig",4:"rat",5:"ox",6:"tiger",7:"rabbit",8:"dragon",9:"snake",10:"horse",11:"sheep"}
+y = a%12
+print(d[y])
--- 3non.py
+++
@@ -1,2 +0,0 @@
-a = int(input("請輸入西元年:"))
-list = ["rat","ox","tiger","rabbit","dragon","snake","horse","sheep","monkey","rooster","dog","pig"]
