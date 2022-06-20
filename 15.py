f6485fc445b241dc4230d1dcf8647efcd29efade
TJH0218 <c110156154@nkust.edu.tw>
2022-06-20 18:32:43 +0800

    1

--- 15.py
+++ 15.py
@@ -1,12 +1,4 @@
 a = input("輸入一個四位數字：")
-# a1 = (int(a[0])+7)%10
-# a2 = (int(a[1])+7)%10
-# a3 = (int(a[2])+7)%10
-# a4 = (int(a[3])+7)%10
-# b = ""
-
-
-# print("輸出加密後的數字為：",b)
 b = []
 for i in range(len(a)):
     s = (int(a[i])+7)%10
@@ -22,6 +14,5 @@ b[3] = t2
 
 b1 = map(str,b)
 c = str(''.join(b1))
-
 
 print("輸出加密後的數字為：",c)
